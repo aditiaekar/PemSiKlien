a14be5414cc2993aa4030390a83ac21a44d80ee9
Aditia Eka <aditiaeka.engineer@gmail.com>
2025-12-05 19:49:21 +0700

    set up vercel

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000",
+  baseURL:
+    "https://pemsiklien-production.up.railway.app" || "http://localhost:4000",
   timeout: 15000,
   headers: {
     "Content-Type": "application/json",
